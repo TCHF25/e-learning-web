1dffa730cba9a34ce1b68768ac59a17029552a41
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-10-09 21:33:53 +0300

    clean all unwanted files

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
-import React from 'react'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Hero />
     </>
   );
 };
 
-
+export default App;
