1c1dfc0057d6a6f2ae254ae78dba2e91e12a2724
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-10-10 19:44:25 +0300

    implementing navbar

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 
 const App = () => {
   return (
     <>
-      <Navbar />
+  
+      <Sidebar />
     </>
   );
 };
